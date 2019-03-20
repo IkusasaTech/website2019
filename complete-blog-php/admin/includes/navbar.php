267d115a629b9998b08563bf4c68d609c0f95d3c
tshepo91 <tshepo@ikusasatech.com>
2019-03-20 15:46:10 +0200

    fix

---
+++ complete-blog-php/admin/includes/navbar.php
@@ -0,0 +1,10 @@
+<div class="header">
+	<div class="logo">
+		<a href="<?php echo BASE_URL .'admin/dashboard.php' ?>">
+			<h1>IkusasaBlog - Admin</h1>
+		</a>
+	</div>
+	<div class="user-info">
+		<span><?php echo $user_id ?></span> &nbsp; &nbsp; <a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
+	</div>
+</div>
--- js/emailValidation.js
+++ js/emailValidation.js
@@ -1,119 +1,38 @@
-//CONTACT FORM VALIDATION
-$(document).ready(function() {
+//Contact Form Validation
+jQuery(document).on('ready', function () {
 
-    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-    //var testNumber = /^[0-9]{10}$/i;
-
-    $('#modalContactForm #name').blur(function() {
-
-        $('#modalContactForm #name').each(function() {
-
-            $this = $(this);
-            if (this.value != '') {
-                successfulInput();
-            } else {
-                errorInput();
-            }
-        });
-    });
-    $('#modalContactForm #email').blur(function() {
-
-        $('#modalContactForm #email').each(function() {
-
-            $this = $(this);
-            if (this.value != '' && testEmail.test($this.val())) {
-                successfulInput();
-            } else {
-                errorInput();
-            }
-        });
-    });
-    /*$('#modalContactForm #phone').blur(function() {
-
-        $('#modalContactForm #phone').each(function() {
-
-            $this = $(this);
-            if (this.value != '' && testNumber.test($this.val())) {
-                successfulInput();
-                $("#emailMessage").removeClass('invalid').addClass('valid').text('Your name is valid.');
-            } else {
-                errorInput();
-                $("#emailMessage").addClass('invalid').removeClass('valid').text('Please add your name.');
-            }
-        });
-    });*/
-    $('#modalContactForm #message').blur(function() {
-
-        $('#modalContactForm #message').each(function() {
-            $this = $(this);
-            if (this.value != '') {
-                successfulInput();
-            } else {
-                errorInput();
-            }
-        });
-    });
-
-    function successfulInput() {
-        $this.removeClass("invalid").addClass("valid");
-    }
-
-    function errorInput() {
-        $this.addClass("invalid").removeClass("valid");
-
-    }
-
-    $('#contactUsFormSubmit').on('click', function() {
-            alert('in');
-            var data = {
-                name: $("#name").val(),
-                email: $("#email").val(),
-                subject: $("#subject").val(),
-                message: $("#message").val()
-            };
-            $.ajax({
-                type: "POST",
-                url: "email.php",
-                data: data,
-                success: function() {
-                    $('#modalContactForm').modal('hide');
-                    $('html, body').animate({ scrollTop: (0) }, '5000');
-                    setTimeout(function() {
-                        alert('success');
-                    }, 500);
-                }
-            });
-            alert('done');
-            $('#name, #email, #subject, #message').removeClass("invalid").removeClass("valid");
-            $('#name').val('').removeClass('valid');
-            $('#email').val('').removeClass('valid');
-            //$('#phone').val('').removeClass('valid');
-            $('#message').val('').removeClass('valid');
-            $('#modalContactForm form label').removeClass('active');
-
-            return false;
-
+  var form = $('.contact__form'),
+      message = $('.contact__msg'),
+      form_data;
+  // Success function
+  function done_func(response) {
       
-    });
-    $('#clientName').on('click', function() {
-        $('#name').val($('#inputName').val());
-        if ($('#name').val() == "") {
-            $('#modalContactForm .md-form').eq(0).find('label').removeClass('active');
-        } else {
-            $('#modalContactForm .md-form').eq(0).find('label').addClass('active');
-        }
-    });
-    $('.contact').click(function() {
-        $('#modalContactForm').modal('show');
-    });
-
-    $('#pushDownNotifications').slideUp();
-    setTimeout(function() {
-        $('#pushDownNotifications').closest(".notification").removeClass("hide");
-    }, 500);
-
-    $('.close-Notification').on('click', function() {
-        $('#pushDownNotifications').slideUp();
-    });
-
+      message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+      message.text(response);
+      setTimeout(function () {
+          message.fadeOut();
+      }, 2000);
+      form.find('input:not([type="submit"]), textarea').val('');
+  }
+  // fail function
+  function fail_func(data) {
+      message.fadeIn().removeClass('alert-success').addClass('alert-success');
+      message.text(data.responseText);
+      setTimeout(function () {
+          message.fadeOut();
+      }, 2000);
+  }
+  
+  form.submit(function (e) {
+      e.preventDefault();
+      form_data = $(this).serialize();
+      $.ajax({
+          type: 'POST',
+          url: form.attr('action'),
+          data: form_data
+      })
+      .done(done_func)
+      .fail(fail_func);
+  });
+ 
 });
